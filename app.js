0b73c2e94ede6cfbafa23165b5630f210e834e6b
sowonnamoo <sowonnamoo@gmail.com>
2026-06-03 13:26:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ function renderTable() {
         const d = data.createdAt.toDate();
         const dateStr = `${d.getFullYear()}-${String(d.getMonth()+1).padStart(2,'0')}-${String(d.getDate()).padStart(2,'0')}`;
         listBody.innerHTML += `<tr class="hover:bg-gray-50 border-b cursor-pointer text-center text-gray-700" onclick="viewDetail('${data.id}')">
-            <td class="py-3 px-4 text-left font-medium text-gray-900 hover:underline">🔒 ${data.productName} [주문접수].</td>
+            <td class="py-3 px-4 text-left font-medium text-gray-900 hover:underline">🔒 ${data.productName} (접수완료)</td>
             <td class="py-3 text-sm text-gray-600">${data.author || "익명"}</td>
             <td class="py-3 text-xs text-gray-400">${dateStr}</td></tr>`;
     });
