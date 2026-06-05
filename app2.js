ee0754e8f87abdbf4c2e84e1e2d09966fcad05b8
sowonnamoo <sowonnamoo@gmail.com>
2026-06-05 18:43:23 +0900

    Update app2.js

--- app2.js
+++ app2.js
@@ -46,7 +46,7 @@ function renderTable(dataToRender = allOrders) {
     <tr class="hover:bg-gray-50 border-b border-gray-100"> 
         <td class="py-3 px-4 text-left font-medium text-gray-900 truncate">
             <span class="mr-2">🔒 ${data.author}님</span>
-            <button onclick="viewDetail('${data.id}')" class="bg-blue-600 text-white text-[10px] px-2 py-0.5 rounded-full mr-2 hover:bg-blue-700">시안요청완료/작업상황</button>
+            <button onclick="viewDetail('${data.id}')" class="bg-blue-600 text-white text-[10px] px-2 py-0.5 rounded-full mr-2 hover:bg-blue-700">시안요청완료 / 작업진행상황</button>
             <span class="text-xs text-gray-500">${displayInfo}</span>
         </td>
         <td class="py-3 text-sm text-gray-600">관리자</td>
